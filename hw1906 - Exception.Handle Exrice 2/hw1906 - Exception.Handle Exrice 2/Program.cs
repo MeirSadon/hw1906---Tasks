b5038cf19c83d14b949787eac53a350ffdc353ec
מאיר סעדון <45739995+MeirSadon@users.noreply.github.com>
2019-06-23 20:21:40 +0300

    Add files via upload

---
+++ hw1906 - Exception.Handle Exrice 2/hw1906 - Exception.Handle Exrice 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw1906___Exception.Handle_Exrice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+            {
+                List<int> numbers = new List<int>(3);
+                Console.WriteLine(numbers[4]);
+            });
+            Thread.Sleep(2000);
+            if (t.IsFaulted)
+            {
+                t.Exception.Handle((e) =>
+                {
+                    Console.WriteLine(e);
+                    return true;
+                });
+            }
+        }
+    }
+}
